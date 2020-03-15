a319d123f6f0bae7f28481fda335a709ca9186fa
chloeconnor <chloe.connor@vortexa.com>
2020-03-15 12:54:57 +0000

    greeting from list

--- src/clojure_exercises/core.clj
+++ src/clojure_exercises/core.clj
@@ -1,13 +1,23 @@
 (ns clojure-exercises.core)
 
+(defrecord Person [first-name last-name])
+
 (defn add-three-numbers [one, two, three]
   (+' one two three))
 
-(defn print-greeting [name, apples]
-  (let [new (apply str "Hello " name ", here's " apples " apples!")]
-  (println new)))
+(defn print-greeting [greeting, name, apples]
+  (
+    let [new (apply str greeting " " name ", here's " apples " apples!")]
+    (println new)
+    ))
 
-(defn -main
-  []
-  (let [apples (add-three-numbers 1 2 3)]
-  (print-greeting  "Chloe" apples)))
+(defn add-element-to-list [list]
+  (conj list "hi" "hello"))
+
+(defn -main []
+  (
+    let [apples (add-three-numbers 1 2 3),
+         chloe-person (Person. "chloe" "connor")
+         greeting (first (add-element-to-list ["yo"]))]
+    (print-greeting greeting (:first-name chloe-person) apples)))
+
