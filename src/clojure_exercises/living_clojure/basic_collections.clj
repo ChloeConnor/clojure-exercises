b31db1c66a817845c4e8cb451288bcd0bd91b241
chloeconnor <chloe.connor@vortexa.com>
2020-03-30 08:35:15 +0100

    map

---
+++ src/clojure_exercises/living_clojure/basic_collections.clj
@@ -0,0 +1,32 @@
+(ns clojure-exercises.living-clojure.basic-collections)
+
+(defn get-second-element [my-list]
+  (first (rest my-list))
+  )
+
+(defn add-to-vector [my-vector]
+  (let [last-element (last my-vector)
+        plus-one (+ last-element 1)]
+    (conj my-vector plus-one)
+    ))
+
+(defn get-a-surname [people-map first-name]
+  (let [new-map (assoc (merge people-map {:julie "bellm" :cas "connor"}) :ben "connor")]
+    (first-name new-map)
+    ))
+
+(defn count-unique-vals [people-map]
+  (let [list-values (vals people-map)
+        set-values (set list-values)]
+    (count set-values)
+    ))
+
+(defn -main []
+  (let [my-list '(:chloe :joe :ben :cas :john)
+        my-vector [1 2 3 4]
+        people-map {:chloe "connor", :joe "bellm"}]
+    (println (get-a-surname people-map :chloe))
+    (println (count-unique-vals people-map))
+    (println (get-second-element my-list))
+    (println (add-to-vector my-vector))
+    ))
---
+++ src/clojure_exercises/living_clojure/flow_and_functional_transformations.clj
@@ -0,0 +1,52 @@
+(ns clojure-exercises.living-clojure.flow-and-functional-transformations
+  (:require [clojure-exercises.living-clojure.basic-collections :as bc])
+  (:require [clojure.string :as cs]))
+
+(defn connor? [people-map first-name]
+  (= (bc/get-a-surname people-map first-name) "connor"))
+
+(defn what-family [first-name people-map]
+  (let [surname (bc/get-a-surname people-map first-name)]
+    (cond
+      (= surname "bellm") "Bellm family"
+      (= surname "connor") "Connor family"
+      :else "Not a Connor or a Bellm"
+      ))
+  )
+
+(defn in-a-relationship-with [first-name]
+  (str "You are in a relationship with " first-name)
+  )
+
+(defn what-partner [first-name partner-map]
+  (first-name partner-map)
+  )
+
+(defn find-relationship [first-name partner-map]
+  ((comp in-a-relationship-with what-partner) first-name partner-map)
+  )
+
+(defn get-some-random-numbers [how-many]
+  (let [take-number (* how-many 3)]
+    (take take-number (cycle (take how-many (repeatedly #(rand-int 10)))))
+    ))
+
+(defn chloe [people-map]
+  ((partial what-family :chloe) people-map))
+
+(defn get-full-name [names-list people-map]
+  (map  #(str (cs/replace % ":" "") " " (people-map %)) names-list)
+  )
+
+(defn -main []
+  (let [people-map {:chloe "connor", :joe "bellm"}
+        another-people-map {:chloe "bellm", :joe "bellm"}
+        partner-map {:chloe :joe, :cas :john, :julie :mark}
+        names-list '(:chloe :joe)]
+    (println (connor? people-map :chloe))
+    (println (chloe another-people-map))
+    (println (what-family :chloe people-map))
+    (println (find-relationship :cas partner-map))
+    (println (get-some-random-numbers 3))
+    (println (get-full-name names-list people-map))
+    ))
