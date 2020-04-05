e7cf526cbd87b5d2fe98fbec489a7a69702c7364
chloeconnor <chloe.connor@vortexa.com>
2020-04-05 11:39:28 +0100

    add some stuff from chapter 3

---
+++ src/clojure_exercises/living_clojure/java_interop_and_polymorphism.clj
@@ -0,0 +1,6 @@
+(ns clojure-exercises.living-clojure.java-interop-and-polymorphism)
+
+
+(def chloe (String. "chloe"))
+
+(println chloe)
---
+++ src/clojure_exercises/living_clojure/state_and_concurrency.clj
@@ -0,0 +1,38 @@
+(ns clojure-exercises.living-clojure.state-and-concurrency)
+
+;Atom Synchronous Uncoordinated
+;Ref Synchronous Coordinated
+;Agent Asynchronous Uncoordinated
+
+
+(def glass-atom (atom :empty))
+
+(defn fill-up [state]
+  (case state
+    :empty :half-full
+    :half-full :full
+    :full))
+
+(println (swap! glass-atom fill-up))
+
+
+(def fullness (ref 0))
+(def pour-water (ref 7))
+
+(defn pour-some-water []
+  (dosync (when (pos? @pour-water)
+            (alter pour-water dec)
+            (alter fullness #(+ % 5)))))
+(let [n 1]
+  (future (dotimes [_ n] (pour-some-water)))
+  (future (dotimes [_ n] (pour-some-water)))
+  (future (dotimes [_ n] (pour-some-water))))
+
+(println @fullness)
+(println @pour-water)
+
+;agent will only process one action at a time
+(def pint-agent (agent :empty))
+
+(println @(send pint-agent fill-up))
+
