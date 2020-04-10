90ae318e9e3f0628bbf06afecc61ab30bd7ba8ae
chloeconnor <chloe.connor@vortexa.com>
2020-04-10 13:20:19 +0100

    add order

--- src/clojure_exercises/quiz_categories.clj
+++ src/clojure_exercises/quiz_categories.clj
@@ -4,9 +4,11 @@
 
 (def people-list '(:julie :mark :cas :ben :chloe :joe))
 
+(def order '(1 2 3 4 5 6 7))
+
 (defn -main []
   (let [shuffle-cats (shuffle categories-list)
-        shuffle-people (shuffle people-list)
-        ]
-    (println (zipmap shuffle-people shuffle-cats)))
+        shuffle-people (shuffle people-list)]
+    (println (zipmap shuffle-people shuffle-cats))
+    (println (zipmap order (conj shuffle-people :john))))
   )
